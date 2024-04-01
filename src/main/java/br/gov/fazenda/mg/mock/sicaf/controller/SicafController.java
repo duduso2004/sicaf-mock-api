76504fdbf1719f515f74c43b9bcdba81496b16e3
duduso <oliveira.eduardo@fazenda.mg.gov.br>
2024-04-01 08:56:18 -0300

    ajustes para melhoria de performance

--- src/main/java/br/gov/fazenda/mg/mock/sicaf/controller/SicafController.java
+++ src/main/java/br/gov/fazenda/mg/mock/sicaf/controller/SicafController.java
@@ -2,7 +2,6 @@ package br.gov.fazenda.mg.mock.sicaf.controller;
 
 import br.gov.fazenda.mg.mock.sicaf.dto.SicafResponseDTO;
 import br.gov.fazenda.mg.mock.sicaf.service.MockService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,16 +17,12 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/obterStatusVeiculoSicaf")
 public class SicafController {
 
-    private final ObjectMapper mapper;
     private final MockService mockService;
-
-    private static final String JSON_PATH_PATTERN = "/%s.json";
-    private static final String RESPONSE_DEFAULT = "/default.json";
 
     @SneakyThrows
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public Mono<SicafResponseDTO> obterStatusVeiculoSicaf(@RequestParam Long renavam) {
-        return Mono.just(this.mapper.readValue(this.mockService.recuperarRetorno(renavam), SicafResponseDTO.class));
+        return Mono.just(this.mockService.recuperarRetorno(renavam));
     }
 
 }
--- src/main/java/br/gov/fazenda/mg/mock/sicaf/service/MockService.java
+++ src/main/java/br/gov/fazenda/mg/mock/sicaf/service/MockService.java
@@ -1,5 +1,9 @@
 package br.gov.fazenda.mg.mock.sicaf.service;
 
+import br.gov.fazenda.mg.mock.sicaf.dto.SicafResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 import java.net.URL;
@@ -9,26 +13,30 @@ import java.util.Map;
 import static java.util.Optional.ofNullable;
 
 @Service
+@RequiredArgsConstructor
 public class MockService {
 
     private static final String JSON_PATH_PATTERN = "/%s.json";
     private static final String RESPONSE_DEFAULT = "/default.json";
 
-    private final Map<Long, URL> mapaRetornos = new HashMap<>();
-    private final URL retornoDefault = getFileUrl(RESPONSE_DEFAULT);
+    private final ObjectMapper mapper;
+    private final Map<Long, SicafResponseDTO> mapaRetornos = new HashMap<>();
 
+    @SneakyThrows
     public void carregarMapaRetornos() {
-        this.mapaRetornos.put(210415533L, getFileUrl(JSON_PATH_PATTERN.formatted(210415533L)));
-        this.mapaRetornos.put(239900049L, getFileUrl(JSON_PATH_PATTERN.formatted(239900049L)));
-        this.mapaRetornos.put(241755352L, getFileUrl(JSON_PATH_PATTERN.formatted(241755352L)));
-        this.mapaRetornos.put(902031031L, getFileUrl(JSON_PATH_PATTERN.formatted(902031031L)));
-        this.mapaRetornos.put(911245774L, getFileUrl(JSON_PATH_PATTERN.formatted(911245774L)));
-        this.mapaRetornos.put(1304958024L, getFileUrl(JSON_PATH_PATTERN.formatted(1304958024L)));
-        this.mapaRetornos.put(1365265193L, getFileUrl(JSON_PATH_PATTERN.formatted(1365265193L)));
+        this.mapaRetornos.put(0L, this.mapper.readValue(getFileUrl(RESPONSE_DEFAULT), SicafResponseDTO.class));
+        this.mapaRetornos.put(210415533L, this.mapper.readValue(getFileUrl(JSON_PATH_PATTERN.formatted(210415533L)), SicafResponseDTO.class));
+        this.mapaRetornos.put(239900049L, this.mapper.readValue(getFileUrl(JSON_PATH_PATTERN.formatted(239900049L)), SicafResponseDTO.class));
+        this.mapaRetornos.put(241755352L, this.mapper.readValue(getFileUrl(JSON_PATH_PATTERN.formatted(241755352L)), SicafResponseDTO.class));
+        this.mapaRetornos.put(902031031L, this.mapper.readValue(getFileUrl(JSON_PATH_PATTERN.formatted(902031031L)), SicafResponseDTO.class));
+        this.mapaRetornos.put(911245774L, this.mapper.readValue(getFileUrl(JSON_PATH_PATTERN.formatted(911245774L)), SicafResponseDTO.class));
+        this.mapaRetornos.put(1304958024L, this.mapper.readValue(getFileUrl(JSON_PATH_PATTERN.formatted(1304958024L)), SicafResponseDTO.class));
+        this.mapaRetornos.put(1365265193L, this.mapper.readValue(getFileUrl(JSON_PATH_PATTERN.formatted(1365265193L)), SicafResponseDTO.class));
     }
 
-    public URL recuperarRetorno(Long renavam) {
-        return ofNullable(this.mapaRetornos.get(renavam)).orElse(this.retornoDefault);
+    public SicafResponseDTO recuperarRetorno(Long renavam) {
+        final var responseDefault = this.mapaRetornos.get(0L);
+        return ofNullable(this.mapaRetornos.get(renavam)).orElse(responseDefault);
     }
 
     private URL getFileUrl(String file) {
